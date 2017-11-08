66fced492f41d8c287e7d3a895001b853e76b4e6
dickanirwansyah <dickanirwansyah@gmail.com>
2017-11-08 18:26:46 +0700

    employee repository

---
+++ commandSpringbootRepository/src/main/java/command/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package command.repository;
+
+import command.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface EmployeeRepository extends JpaRepository<Employee, String>{
+}
